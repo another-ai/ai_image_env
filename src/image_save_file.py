7677e76b0f5d8036727957416650ee0e3b15c8ad
another-ai <maxencry@gmail.com>
2024-02-06 23:10:38 +0100

    Update image_save_file.py

--- src/image_save_file.py
+++ src/image_save_file.py
@@ -99,7 +99,7 @@ def save_file_alt(image_file, txt_file_data_file, directory_save,env_file_text):
         unique_id = count_file(directory_path)
         file_name = f"{unique_id}_{current_time}.png"
         file_path = f"{directory_path}/{file_name}"
-        image_file = constrast_image(image_file, 1.6)
+        image_file = constrast_image(image_file, 1.1)
         image_file.save(file_path)
         add_metadata_file(file_path, txt_file_data_file, env_file_text)
     return file_path
